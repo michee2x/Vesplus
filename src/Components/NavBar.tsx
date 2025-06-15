977453b6c343fd01f8abfa1b7ef3ec4cccf64f4b
michee2x <michaelisraelmike@gmail.com>
2025-06-15 07:52:13 +0100

    commit

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,13 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {IoMdMenu} from "react-icons/io"
 import {LiaTimesSolid} from "react-icons/lia"
 
 const NavBar = () => {
     const [showSideBar, setShowSideBar] = useState(false)
+    const audioElement = useRef<HTMLAudioElement | null>(null);
+    const [isAudioPlaying, setIsAudioPlaying] = useState(true);
+    const [isIndicatorActive, setIndicatorActive] = useState(true);
+
+    const toggleAudioIndicator = () => {
+          setIsAudioPlaying(prev => !prev);
+          setIndicatorActive(prev => !prev);
+
+    }
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const audio = audioElement.current;
+        if (!audio) return;
+
+        audio
+          .play()
+          .then(() => {
+            console.log("Audio playing!");
+          })
+          .catch((err) => {
+            console.warn("Autoplay blocked:", err);
+          });
+
+        // Remove listener after first scroll
+        window.removeEventListener("scroll", handleScroll);
+      };
+
+      // Add listener on mount
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+    
+
+
+    useEffect(()=> {
+      if(!audioElement.current) return;
+        if(isAudioPlaying){
+          audioElement.current.play()
+        } else {
+          audioElement.current.pause()
+        }
+    }, [isAudioPlaying])
   return (
     <div className="w-screen z-[4000] fixed flex flex-col lg:flex-row justify-between lg:items-start items-center lg:px-6 py-2 lg:py-4 h-auto">
       <div className="flex z-10 px-3 justify-between items-center w-full lg:w-fit h-auto">
-        <img src="https://i.imgur.com/G7oYvV1.png" className='w-10 h-10 object-cover rounded-full' alt="" />
+        <img
+          src="https://i.imgur.com/G7oYvV1.png"
+          className="w-10 h-10 object-cover rounded-full"
+          alt=""
+        />
         <span
           onClick={() => setShowSideBar((prev) => !prev)}
           className="text-3xl  lg:hidden text-zinc-100"
@@ -25,6 +75,39 @@ const NavBar = () => {
         <li>Careers</li>
         <li>PR Centers</li>
         <li>Contact</li>
+        <button
+          onClick={toggleAudioIndicator}
+          className="flex gap-1 items-end h-10"
+        >
+          <audio
+            className="hidden"
+            ref={audioElement}
+            src="/BDASH%20P&Pm%20Soundtrack%20(WOD)%20-%20LovesMusic.mp3"
+            autoPlay
+            muted
+            loop
+            preload="auto"
+          ></audio>
+
+          {[1, 2, 3, 4].map((bar) => {
+            // Generate random delay and duration for each bar
+            const randomDelay = (Math.random() * 0.5).toFixed(2); // 0s to 0.5s
+            const randomDuration = (0.8 + Math.random()).toFixed(2); // 0.8s to 1.8s
+
+            return (
+              <div
+                key={bar}
+                className={`indicator-line transition-all ${
+                  isIndicatorActive ? "opacity-100" : "h-[40%]"
+                }`}
+                style={{
+                  animationDelay: `${randomDelay}s`,
+                  animationDuration: `${randomDuration}s`,
+                }}
+              ></div>
+            );
+          })}
+        </button>
       </ul>
     </div>
   );
